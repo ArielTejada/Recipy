952a986b3bfc1118d073b6390ada21546323f3e9
Ariel Tejada <85589271+ArielTejada@users.noreply.github.com>
2022-09-13 14:03:58 -0400

    3rd divided styles into sepereate js files /styles

--- recipy/screens/home.js
+++ recipy/screens/home.js
@@ -1,15 +1,12 @@
 import React from "react"
 import {Text, View, StyleSheet} from "react-native"
+import styles from '../styles/home-styles'
 
 export default function Home() {
 
   return (
     <View>
-      <Text>Home</Text>
+      <Text style={styles.container}>Home</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-    
-})
---
+++ recipy/styles/settings-styles.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {StyleSheet} from "react-native"
+
+export default StyleSheet.create({
+    container: {
+        fontSize: 24,
+        padding: 10,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor: '#43a1c9',
+    }
+})
