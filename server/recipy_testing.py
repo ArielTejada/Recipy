a1fbeef86fe35e1e6cd71b3defae272fc366e056
Christian Gillies <ccrg4@hotmail.com>
2022-10-15 22:14:17 -0400

    Reached Index 226. Database almost complete.

--- server/recipy_testing.py
+++ server/recipy_testing.py
@@ -341,7 +341,7 @@ recipe_data=recipe_data.drop(['Unnamed: 0'], axis=1)
 #print(recipe_data)
 recipe_data.to_csv(path_to_central_recipe_data)
 
-starting_index = 10 # IMPORTANT: Change this value to start later. Currently at 10. You need to note the index when you stop running program
+starting_index = 226 # IMPORTANT: Change this value to start later. You need to note the index when you stop running program
 
 for i in range(starting_index, len(ingredient_data)):
     query = ingredient_data[i]
