9f9e2626222f256527faf826f67b8e11bf6de55d
MatthewMing11 <Matthew.Ming11@myhunter.cuny.edu>
2022-11-03 01:45:52 -0400

    adding testing notification stuff

--- recipy_V2/screens/addIngredient.js
+++ recipy_V2/screens/addIngredient.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {StyleSheet, Button, Text, View, Image, TouchableWithoutFeedback, TouchableOpacity, Keyboard, ScrollView, ImageBackground, Pressable} from "react-native";
+import {StyleSheet, Button, Text, View, Image, TouchableWithoutFeedback, TouchableOpacity, Keyboard, ScrollView, ImageBackground, Pressable, TextInput } from "react-native";
 import styles from '../styles/add-styles';
 import { useStoreState, useStoreActions } from "easy-peasy";
 
@@ -7,12 +7,22 @@ import { useStoreState, useStoreActions } from "easy-peasy";
 import SearchBar from "../components/searchBar"
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
+import * as Notifications from 'expo-notifications';
+import { StatusBar } from 'expo-status-bar';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
 export default function AddIngredient({navigation}) {
 
 /* -------------------- Local State Variables -------------------- */
 const [hasPermission, setHasPermission] = useState(null);
 const [scanned, setScanned] = useState(false);
 const [shouldShow, setShouldShow] = useState(false);
+const [filler,setFiller]= useState("");
+const [text,setText] =useState("change me");
+const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+const [date,setDate] = useState(new Date(1666870995000));
+const [output,setOutput]= useState("");
+const [time,setTime]=useState(0);
 
 /* -------------------- Redux State Variables -------------------- */
 const selectedIngredients = useStoreState(state => state.selectedIngredients);
@@ -60,6 +70,52 @@ if (hasPermission === false){
 const onPress = () => {
   setScanned(false);
   setShouldShow(!shouldShow);
+}
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+const showDatePicker = () => {
+  setDatePickerVisibility(true);
+};
+
+const hideDatePicker = () => {
+  setDatePickerVisibility(false);
+};
+
+const handleConfirm = (date) => {
+  console.warn("A date has been picked: ", date);
+  setDate(date);
+  // setOutput(date.getTime());
+  // setTime(date.getTime()-Date.now()-(48*60*60*1000))
+  setTime(date.toLocaleDateString())
+  hideDatePicker();
+};
+
+const updateText = () => {
+  setText(filler);
+}
+
+async function schedulePushNotification(filler,time) {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got a notification! 📬",
+      body: 'Your '+ filler +' is expiring in two days! Better use it soon!',
+      data: { data: 'goes here' },
+    },
+    trigger: { 
+      // repeats: false,
+      // weekday: 2,
+      // hour: 4,
+      // minute: 2,
+      seconds:2
+    },
+  });
 }
 
 const selectedListPress = (key) => {
@@ -171,6 +227,22 @@ const selectedListPress = (key) => {
         style={StyleSheet.absoluteFillObject}
       /> : null}
       {scanned && <Button title = {'Tap to Return'} onPress ={onPress}/>}
+      <Text>Enter your Ingredient below!</Text>
+      {/* <Text>there is no {text}</Text> */}
+      <TextInput placeholder="Ingredient"
+      onChangeText={(value) => setFiller(value)}/>
+      <StatusBar style="auto" />
+      {/* <Button onPress={()=>updateText()} title="click here"/>  */}
+      <Text>{date.toLocaleDateString()}</Text>
+      <Button title="Pick Date" onPress={showDatePicker} />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        minimumDate={Date.now()-24*60*60*1000}
+      />
+      <Button onPress={() =>schedulePushNotification(filler,time)}title="click here">Click me</Button>
     </View>
   );
 }
