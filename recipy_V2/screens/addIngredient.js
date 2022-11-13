e5f8d39d27bc940e4a6548d89c60b954cb8d46f5
MatthewMing11 <Matthew.Ming11@myhunter.cuny.edu>
2022-11-13 17:09:52 -0500

    confusion about selectedIngredients

--- recipy_V2/screens/addIngredient.js
+++ recipy_V2/screens/addIngredient.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import {StyleSheet, Button, Text, View, Image, TouchableWithoutFeedback, TouchableOpacity, Keyboard, ScrollView, ImageBackground, Pressable} from "react-native";
+import {FlatList, StyleSheet, Button, Text, View, Image, TouchableWithoutFeedback, TouchableOpacity, Keyboard, ScrollView, ImageBackground, Pressable} from "react-native";
 import styles from '../styles/add-styles';
 import { useStoreState, useStoreActions } from "easy-peasy";
 
 /* -------------------- Components -------------------- */
 import SearchBar from "../components/searchBar"
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { SafeAreaInsetsContext } from "react-native-safe-area-context";
 
 export default function AddIngredient({navigation}) {
 
@@ -13,10 +14,12 @@ export default function AddIngredient({navigation}) {
 const [hasPermission, setHasPermission] = useState(null);
 const [scanned, setScanned] = useState(false);
 const [shouldShow, setShouldShow] = useState(false);
+const [json, setJson] = useState([]);
 
 /* -------------------- Redux State Variables -------------------- */
+const ingredients = useStoreState(state => state.ingredients);
 const selectedIngredients = useStoreState(state => state.selectedIngredients);
-const setSelectedIngredients = useStoreActions(actions => actions.setSelectedIngredients);
+const setSelectedIngredients = useStoreActions(actions => actions.setSelectedIngredients);//use this
 const refresh = useStoreState(state => state.refresh);
 const setRefresh = useStoreActions(actions => actions.setRefresh);
 const setHaveIngredients = useStoreActions(actions => actions.setHaveIngredients);
@@ -39,9 +42,39 @@ useEffect(() => {
   getBarCodeScannerPermissions();
 }, []);
 
+const getIngredients = async (data) => {
+  try {
+   const response = await fetch('https://api.upcitemdb.com/prod/trial/lookup?upc='+data);
+   const json = await response.json();
+   setJson(json.items);
+ } catch (error) {
+   console.error(error);
+ } 
+}
+
 const handleBarCodeScanned = ({type,data}) => {
   setScanned(true);
   alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  // getIngredients(data);
+  // console.log(json[0]);
+  setScanned(false);
+  setShouldShow(!shouldShow);
+  // console.log(json[0]['title'].toLowerCase());
+  // console.log(ingredients.length);
+  // console.log(validateIngredient(json[0]['title'].toLowerCase(),ingredients));
+  // result = validateIngredient(json[0]['title'].toLowerCase(),ingredients);
+  // if (true){
+  //   // setBarcodeText(result.name);
+  //   setBarcodeText("milk");
+  // }
+  console.log(selectedIngredients);
+  let list =selectedIngredients;
+  // following two give the "undefined is not an object (evaluating 'ingredient.name.replace')"
+  // list.push("milk")
+  // list.push({"brand": "Elmhurst", "category": "Food, Beverages & Tobacco > Beverages > Non-Dairy Milk", "color": "", "currency": "", "description": "INGREDIENTS: FILTERED WATER, HAZELNUTS, CANE SUGAR, ALMONDS, NATURAL FLAVORS, SALT.", "dimension": "", "ean": "0018944000048", "elid": "292315447067", "highest_recorded_price": 33.16, "images": ["https://i5.walmartimages.com/asr/a308e2f8-68d9-46fe-aab3-87de3f09e6d4_1.dc3154bdb9655e531ae9f414af2a8e5e.png?odnHeight=450&odnWidth=450&odnBg=ffffff", "https://target.scene7.com/is/image/Target/GUEST_190cb48b-13da-4f1b-8ef9-d94f62282eea?wid=1000&hei=1000"], "lowest_recorded_price": 4.99, "model": "Elmhurst Milked", "offers": [{"availability": "", "condition": "New", "currency": "", "domain": "walmart.com", "link": "https://www.upcitemdb.com/norob/alink/?id=x2v223z2y253e494v2&tid=1&seq=1668353399&plt=21a2111ce7087ac209384f8df3c5b822", "list_price": "", "merchant": "Wal-Mart.com", "price": 5.98, "shipping": "5.99", "title": "Elmhurst Milked Hazelnut Milk, 32 fl oz", "updated_t": 1581645909}, {"availability": "", "condition": "New", "currency": "", "domain": "target.com", "link": "https://www.upcitemdb.com/norob/alink/?id=x2s263w2x253a484u2&tid=1&seq=1668353399&plt=73fba671d86a72324ecd171da30e2a66", "list_price": "", "merchant": "Target", "price": 4.99, "shipping": "", "title": "Elmhurst Milked Hazelnuts Milk Substitute - 1qt", "updated_t": 1598412001}], "size": "", "title": "MILKED HAZELNUTS", "upc": "018944000048", "weight": ""});
+  // setSelectedIngredients(list);
+  // setHaveIngredients();
+  // setRefresh(!refresh);
 };
 
 if (hasPermission === null){
@@ -52,10 +85,16 @@ if (hasPermission === false){
   return <Text>No access to camera</Text>;
 }
 
-const onPress = () => {
-  setScanned(false);
-  setShouldShow(!shouldShow);
-}
+const validateIngredient = ({ingredient,list}) => {
+  let result = [];
+  for(let i = 0; i < list.length; i++){
+    console.log(list[i].name);
+    if (ingredient.includes(list[i].name)){
+      result.push(list[i]);
+    }
+  }
+  return result;
+};
 
 const selectedListPress = (ingredientObj) => {
   let newList = selectedIngredients.filter((ingredient) => ingredient.id != ingredientObj.id);
@@ -118,7 +157,13 @@ const recentPressHandler = (ingredientObj) => {
         title = "Barcode Scanner" 
         onPress={() => setShouldShow(!shouldShow)}
       />
-      
+      <FlatList
+          data={json}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.upc}</Text>
+          )}
+        />
       <View style={[styles.margins, styles.selected, styles.fontSmall]}>
           <View style={[styles.selectedIngredients, styles.outline]}>
             <ScrollView horizontal={true}>
@@ -171,7 +216,6 @@ const recentPressHandler = (ingredientObj) => {
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       /> : null}
-      {scanned && <Button title = {'Tap to Return'} onPress ={onPress}/>}
     </View>
   );
 }
